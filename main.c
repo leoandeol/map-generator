29ceae560eda7c240dc287e27d138d31c16d4980
Leo Andeol <leo.andeol@gmail.com>
2016-10-13 09:24:03 +0800

    Update but still not working properly

--- main.c
+++ main.c
@@ -10,9 +10,9 @@
 
 // POUR GEN1 : 2pow11 +1
 
-const int WIDTH = 2049;
-const int HEIGHT = 2049;
-const int DEVIATION = 10;
+const int WIDTH = 257;
+const int HEIGHT = 257;
+const int DEVIATION = 40;
 
 const char* WATER = "0 20 255";
 const char* GROUND = "0 100 0";
@@ -27,12 +27,59 @@ int _rand(int min, int max){
 
 void gen1rec(float** t,int x, int y, int mx, int my,int dev)
 {
-	printf("x=%d\ny=%d\nmx=%d\nmy=%d\n\n",x,y,mx,my);
-	t[(x+mx)/2][(y+my)/2]=(t[x][y]+t[mx][y]+t[x][my]+t[mx][my])/4+_rand(-dev,dev);
-	t[x][(y+my)/2]=(t[x][y]+t[x][my]+t[(x+mx)/2][(y+my)/2]+((x-((mx-x)/2)>=0)?t[x-((mx-x)/2)][(y+my)/2]:0))/4+_rand(-dev,dev);
-	t[(x+mx)/2][y]=(t[x][y]+t[mx][y]+t[(x+mx)/2][(y+my)/2]+((y-((my-y)/2)>=0)?t[(x+mx)/2][y-((my-y)/2)]:0))/4+_rand(-dev,dev);
-	t[mx][(y+my)/2]=(t[mx][y]+t[mx][my]+t[(x+mx)/2][(y+my)/2]+((mx+((mx-x)/2)<WIDTH)?t[mx+((mx-x)/2)][(y+my)/2]:0))/4+_rand(-dev,dev);
-	t[(x+mx)/2][my]=(t[x][my]+t[mx][my]+t[(x+mx)/2][(y+my)/2]+((my+((my-y)/2)<HEIGHT)?t[(x+mx)/2][my+((my-y)/2)]:0))/4+_rand(-dev,dev);
+	//printf("x=%d\ny=%d\nmx=%d\nmy=%d\n\n",x,y,mx,my);
+
+	/*      e
+	      a 3 b
+	    g 2 1 4 f
+          c 5 d
+		    h
+	
+
+			 e f g h being theoretically out of scope
+	*/
+	int a = t[x][y];
+	int b = t[mx][y];
+	int c = t[x][my];
+	int d = t[mx][my];
+
+	// halves
+
+	int hx = (mx+x)/2;
+	int hy = (my+y)/2;
+
+	// Gotta test them, 0 by default : will make the edges have a lower altitude;
+
+	int tmp = (y - ((my-y)/2));
+
+	int e = tmp>=0 ? t[hx][tmp] : 0;
+
+	tmp = (x + ((mx-x)/2));
+
+	int f = tmp<WIDTH ? t[tmp][hy] : 0;
+
+	tmp = (x - ((mx-x)/2));
+
+	int g = tmp>=0 ? t[tmp][hy] : 0;
+
+	tmp = (y + ((my-y)/2));
+
+	int h = tmp<HEIGHT ? t[hx][tmp] : 0;
+
+	// Update values
+	
+	int ce = (a + b + c + d)/4 + _rand(-dev,dev);
+
+	t[hx][hy] = ce;
+	
+	t[x][hy] = (ce + a + g + c)/4 + _rand(-dev,dev);
+
+	t[hx][y] = (a + e + b + ce)/4 + _rand(-dev,dev);
+
+	t[mx][hy] = (b + f + d + ce)/4 + _rand(-dev,dev);
+
+	t[hx][my] = (ce + c + d + h)/4 + _rand(-dev,dev);
+    
 	if(mx-x>2)
 	{	   
 		gen1rec(t,x,y,(x+mx)/2,(y+my)/2,dev/2);
@@ -47,13 +94,24 @@ float** gen1(){
 	for(int i = 0; i < WIDTH; i++)
 	{
 		tab[i] = malloc(sizeof(float)*HEIGHT);
-		for(int j; j < HEIGHT; j++)
+		/*for(int j; j < HEIGHT; j++)
 		{
 			tab[i][j]=0;
-		}
-	}   
+		}*/
+	} 
+	tab[0][0]=_rand(-DEVIATION,DEVIATION);
+	tab[WIDTH-1][0]=_rand(-DEVIATION,DEVIATION);
+	tab[0][HEIGHT-1]=_rand(-DEVIATION,DEVIATION);
+	tab[WIDTH-1][HEIGHT-1]=_rand(-DEVIATION,DEVIATION);
 	// never touch corners to avoid crashes
 	gen1rec(tab,0,0,WIDTH-1,HEIGHT-1,DEVIATION);
+    for(int i = 0; i < WIDTH; i++)
+	{
+		for(int j; j < HEIGHT; j++)
+		{
+			tab[i][j]+=150;;
+		}
+	}  
 	return tab;
 }
 
